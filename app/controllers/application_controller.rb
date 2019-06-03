62698b7f30d1ee8697d7bc427059a93ef6d9f25d
Nate Tan <nktan93@gmail.com>
2019-06-03 18:36:22 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,41 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    @article = Article.last
+    erb :new
+  end
+
+  post '/articles/:id' do
+    @article = Article.create(params)
+    #binding.pry
+    erb :show
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by id: params[:id]
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by id: params[:id]
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by id: params[:id]
+    @article.update(title: params[:title], content: params[:content])
+    erb :show
+  end
+
+  delete '/articles/:id/delete' do
+    article = Article.find_by id: params[:id]
+    article.destroy
+  end
 end
